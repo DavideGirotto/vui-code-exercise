b807b0dbc1cbe995fa14cbc88f6ab62fdf552265
Davide Girotto <dgirotto@goresponsa.com>
2022-03-28 01:42:09 +0200

    Added response header to solve CORS issue

--- backend/index.ts
+++ backend/index.ts
@@ -4,6 +4,7 @@ import searchResults from './datasets/searchResults.json'
 const app = express();
 
 app.get('/search_query/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.send(searchResults);
 })
 
