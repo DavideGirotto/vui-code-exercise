a1f2707edc610e36cc77d0a140df4da5a187a9a6
Davide Girotto <dgirotto@goresponsa.com>
2022-03-27 22:54:39 +0200

    Added Features Products component

--- frontend/src/components/featuredProducts/FeauturedProducts.js
+++ frontend/src/components/featuredProducts/FeauturedProducts.js
@@ -8,75 +8,75 @@ import CardActions from '@mui/material/CardActions'
 import Typography from '@mui/material/Typography'
 
 const FeaturedProductsContainer = styled(Box)({
-    marginTop: '32px'
+  marginTop: '32px'
 })
 
 const SectionTitle = styled(Typography)(({ theme }) => ({
-    padding: '32px 0',
-    color: theme.palette.primary.main
+  padding: '32px 0',
+  color: theme.palette.primary.main
 }))
 
 export default function FeaturedProducts () {
   return (
     <FeaturedProductsContainer>
-        <SectionTitle variant='h4'>Featured Products</SectionTitle>
-        <Grid
+      <SectionTitle variant='h4'>Featured Products</SectionTitle>
+      <Grid
         container
         justifyContent='center'
         alignItems='center'
         spacing={4}
-        >
+      >
         <Grid item xs={6} md={4}>
-            <Card sx={{}}>
+          <Card sx={{}}>
             <CardMedia
-                component='img'
-                image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
+              component='img'
+              image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
             />
             <CardContent>
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
             </CardContent>
             <CardActions disableSpacing>
-                Aisle: 5
+              Aisle: 5
             </CardActions>
-            </Card>
+          </Card>
         </Grid>
 
         <Grid item xs={6} md={4}>
-            <Card sx={{}}>
+          <Card sx={{}}>
             <CardMedia
-                component='img'
-                image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
+              component='img'
+              image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
             />
             <CardContent>
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
             </CardContent>
             <CardActions disableSpacing>
-                Aisle: 5
+              Aisle: 5
             </CardActions>
-            </Card>
+          </Card>
         </Grid>
 
         <Grid item xs={6} md={4}>
-            <Card sx={{}}>
+          <Card sx={{}}>
             <CardMedia
-                component='img'
-                image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
+              component='img'
+              image='https://mobileimages.lowes.com/productimages/04e18bf4-0595-43e5-b4f6-f9b5404ad25f/16800441.jpg?sw=80'
             />
             <CardContent>
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
             </CardContent>
             <CardActions disableSpacing>
-                Aisle: 5
+              Aisle: 5
             </CardActions>
-            </Card>
+          </Card>
         </Grid>
-        </Grid>
+      </Grid>
     </FeaturedProductsContainer>
   )
 }
