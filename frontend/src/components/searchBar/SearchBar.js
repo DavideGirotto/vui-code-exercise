0b0da4f760cf0adced12b2fbb48b2e847f6519ce
Davide Girotto <dgirotto@goresponsa.com>
2022-03-29 01:23:49 +0200

    Added product suggestions

--- frontend/src/components/searchBar/SearchBar.js
+++ frontend/src/components/searchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import { styled, alpha } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography';
 import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
@@ -9,23 +9,28 @@ import InputBase from '@mui/material/InputBase'
 import SearchIcon from '@mui/icons-material/Search'
 import Paper from '@mui/material/Paper'
 import Chip from '@mui/material/Chip'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 import Backdrop from '@mui/material/Backdrop'
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAutocompleteSuggestions, getSearchResults, selectAutocompleteSuggestions, selectCategoriesSuggestions } from '../../store/reducers/search'
+import { selectProducts } from '../../store/reducers/products'
 
 const SearchContainer = styled(Box)({
   position: 'relative',
   minHeight: '75px',
-  maxHeight: '560px'
 })
 
 const SearchWrapper = styled(Paper)({
   position: 'absolute',
   borderRadius: '32px',
-  padding: '16px',
+  padding: 16,
   width: 'calc(100% - 32px)',
   border: '1px solid #f5f5f5',
+  maxHeight: '560px',
   zIndex: 10
 })
 
@@ -72,7 +77,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const AvailabilityFilter = styled(FormGroup)(({ theme }) => ({
   display: 'inline-block',
   marginTop: '8px',
-  marginLeft: '16px',
+  marginLeft: 16,
   '& .MuiCheckbox-root': {
     marginRight: '4px'
   },
@@ -85,7 +90,7 @@ const AvailabilityFilter = styled(FormGroup)(({ theme }) => ({
 }))
 
 const CategoriesContainer = styled(Box)({
-  marginTop: '16px',
+  marginTop: 16,
   textAlign: 'center'
 })
 
@@ -97,14 +102,27 @@ const Category = styled(Chip)({
   fontSize: '14px'
 })
 
-const Autocomplete = styled(Grid)({
+const Suggestions = styled(Grid)({
   marginTop: '8px'
 })
 
 const AutocompleteItem = styled(Box)(({ theme }) => ({
-  padding: '16px',
+  padding: 16,
   cursor: 'pointer',
-  borderRadius: '16px',
+  borderRadius: 16,
+  transition: '300ms',
+  '&:hover': {
+    background: 'rgba(0, 0, 0, 0.08)',
+    color: theme.palette.primary.main
+  }
+}))
+
+const Product = styled(ListItem)(({ theme }) => ({
+  alignItems: 'stretch', 
+  columnGap: 20,
+  padding: 16,
+  borderRadius: 16,
+  cursor: 'pointer',
   transition: '300ms',
   '&:hover': {
     background: 'rgba(0, 0, 0, 0.08)',
@@ -118,6 +136,7 @@ export default function SearchBar () {
   const [status, setStatus] = useState('closed')
   const categories = useSelector(selectCategoriesSuggestions)
   const autocompleteSuggestions = useSelector(selectAutocompleteSuggestions)
+  const products = useSelector(selectProducts)
 
   const handleSearch = () => {
     dispatch(getSearchResults())
@@ -132,6 +151,7 @@ export default function SearchBar () {
   const handleSuggestionClick = value => {
     setSearchText(value)
     setStatus('closed')
+    dispatch(getAutocompleteSuggestions(value))
   }
 
   return (
@@ -162,13 +182,45 @@ export default function SearchBar () {
           </CategoriesContainer>}
 
         {status === 'expanded' &&
-          <Autocomplete container>
+          <Suggestions container>
             <Grid item xs={12} sm={6}>
               {autocompleteSuggestions.map((s, index) => <AutocompleteItem key={index} onClick={() => handleSuggestionClick(s.name)}>{s.name}</AutocompleteItem>)}
             </Grid>
 
-            <Grid item xs={12} sm={6} />
-          </Autocomplete>}
+            <Grid item xs={12} sm={6} component={List} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: 0}}>
+              {products.map((p, index) => (
+                <Product key={index} >
+                  <ListItemAvatar>
+                    <Avatar 
+                      variant="rounded" 
+                      src={p.imageUrl} 
+                      alt={p.description}
+                      sx={{ border: '1px solid #eee', width: 96, height: 96 }}
+                    />
+                  </ListItemAvatar>
+                  <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                    <Box>
+                      <Typography sx={{fontSize: 14}}>{p.description.length <= 40 ? p.description : p.description.substring(0, 37) + '...'}</Typography>
+                    </Box>
+
+                    <Box>
+                      <Typography sx={{fontSize: 16, padding:'8px 0', fontWeight:800}}>{p.price}</Typography>
+                    </Box>
+
+                    <Box sx={{display: 'flex', flexGrow: 1, alignItems: 'flex-end', fontSize: 12}}>
+                      <Grid container>
+                        <Grid item xs={4}><strong>Aisle:</strong> {p.aisle}</Grid>
+                        <Grid item xs={4}><strong>Bay:</strong> {p.bay}</Grid>
+                        <Grid item xs={4}><strong>Qty:</strong> {p.availability}</Grid>
+                      </Grid>
+                    </Box>
+                  </Box>
+                </Product>
+              ))}
+            </Grid>
+          </Suggestions>
+        
+        }
       </SearchWrapper>
 
       <Backdrop
