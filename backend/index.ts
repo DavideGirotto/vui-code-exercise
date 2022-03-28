85ee93223eb551bebc647492b1f7022218ae9de1
Davide Girotto <dgirotto@goresponsa.com>
2022-03-28 22:44:21 +0200

    Added mock autocomplete suggestions to the backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import searchResults from './datasets/searchResults.json'
+import autocompleteSuggestions from './datasets/autocompleteSuggestions.json'
 
-const app = express();
+const app = express();  
 
-app.get('/search_query/', (req, res) => {
+app.get('/search/', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.send(searchResults);
 })
+
+app.get('/autocomplete/', (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.send(autocompleteSuggestions);
+})
+
 
 app.listen(3080, () => {})
