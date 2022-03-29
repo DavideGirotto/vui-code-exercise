b8704a046237284b73ac2503bcede235fb7f7e58
Davide Girotto <dgirotto@goresponsa.com>
2022-03-29 13:55:36 +0200

    Refactored availability filter

--- frontend/src/components/categories/Categories.js
+++ frontend/src/components/categories/Categories.js
@@ -1,29 +1,14 @@
-import styled from '@mui/system/styled'
-import Box from '@mui/material/Box'
-import Chip from '@mui/material/Chip'
-import { useSelector } from 'react-redux'
 import { selectCategoriesSuggestions } from '../../store/reducers/search'
+import { useSelector } from 'react-redux'
+import { CategoriesContainer, Category } from '../searchBar/SearchBar.styles'
 
-export const CategoriesContainer = styled(Box)({
-  marginTop: 16,
-  textAlign: 'center'
-})
-
-export const Category = styled(Chip)({
-  marginRight: '8px',
-  marginBottom: '8px',
-  cursor: 'pointer',
-  height: '44px',
-  fontSize: '14px'
-})
-
-export default function Categories ({ handleSuggestionClick }) {
+export default function Categories ({ handleClick }) {
   const categories = useSelector(selectCategoriesSuggestions)
 
   return (
     <CategoriesContainer>
       {categories.map((category, index) => (
-        <Category key={index} label={category} onClick={() => handleSuggestionClick(category)} />
+        <Category key={index} label={category} onClick={() => handleClick(category)} />
       ))}
     </CategoriesContainer>
   )
--- frontend/src/components/searchBar/SearchBar.js
+++ frontend/src/components/searchBar/SearchBar.js
@@ -1,106 +1,51 @@
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
-import SearchIcon from '@mui/icons-material/Search'
-import CloseIcon from '@mui/icons-material/Close'
 import Backdrop from '@mui/material/Backdrop'
-import React, { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { getAutocompleteSuggestions, getSearchResults, resetSearch } from '../../store/reducers/search'
 import SearchSuggestions from '../searchSuggestions/SearchSuggestions'
-import { AvailabilityFilter, CloseIconWrapper, SearchContainer, SearchField, SearchIconWrapper, SearchWrapper, StyledInputBase } from './SearchBar.styles'
+import { SearchContainer, SearchWrapper } from './SearchBar.styles'
 import Categories from '../categories/Categories'
+import AvailabilityFilter from '../availabilityFilter/AvailabilityFilter'
+import SearchField from '../searchField/SearchField'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  getAutocompleteSuggestions,
+  getSearchResults,
+  selectSearchBarStatus,
+  setSearchBarStatus,
+  setSearchText
+} from '../../store/reducers/search'
 
 export default function SearchBar () {
   const dispatch = useDispatch()
-  const [searchText, setSearchText] = useState('')
-  const [status, setStatus] = useState('closed')
-  const [availabilityFilter, setAvailabilityFilter] = useState(false)
+  const status = useSelector(selectSearchBarStatus)
 
-  const closeSearchBar = () => {
-    if (window.innerWidth <= 899) { document.getElementsByTagName('body')[0].style.overflow = 'auto' }
-    setStatus('closed')
-  }
-
-  const handleReset = () => {
-    closeSearchBar()
-    setSearchText('')
-    dispatch(resetSearch())
-  }
-
-  const handleSearch = () => {
-    closeSearchBar()
-    dispatch(getSearchResults(availabilityFilter))
-  }
-
-  const handleFocus = () => {
-    if (window.innerWidth <= 899) { document.getElementsByTagName('body')[0].style.overflow = 'hidden' }
-    searchText === '' ? setStatus('open') : setStatus('expanded')
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    closeSearchBar()
-    dispatch(getSearchResults(availabilityFilter))
-  }
-
-  const handleTextChange = value => {
-    setStatus('expanded')
-    setSearchText(value)
-    dispatch(getAutocompleteSuggestions(value))
-  }
+  useEffect(() => {
+    if (status === 'closed' && window.innerWidth <= 899) { document.getElementsByTagName('body')[0].style.overflow = 'auto' }
+  }, [status])
 
   const handleSuggestionClick = value => {
-    setSearchText(value)
-    closeSearchBar()
-    dispatch(getSearchResults(value))
+    dispatch(setSearchText(value))
+    dispatch(setSearchBarStatus('closed'))
+    dispatch(getSearchResults())
     dispatch(getAutocompleteSuggestions(value))
   }
 
   return (
     <SearchContainer>
       <SearchWrapper elevation={status !== 'closed' ? 3 : 0}>
-        <SearchField sx={status !== 'closed' ? { border: '2px solid #264990' } : {}}>
-          <form onSubmit={handleSubmit}>
-            <StyledInputBase
-              placeholder='Search for a product or brand…'
-              value={searchText}
-              onFocus={handleFocus}
-              onChange={e => handleTextChange(e.target.value)}
-              onSubmit={handleSearch}
-            />
-          </form>
 
-          {searchText !== '' &&
-            <CloseIconWrapper size='small' onClick={handleReset}>
-              <CloseIcon fontSize='small' />
-            </CloseIconWrapper>}
+        <SearchField />
 
-          <SearchIconWrapper onClick={handleSearch}>
-            <SearchIcon />
-          </SearchIconWrapper>
-        </SearchField>
+        <AvailabilityFilter />
 
-        <AvailabilityFilter>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={availabilityFilter}
-                onChange={e => setAvailabilityFilter(e.target.checked)}
-              />
-            }
-            label='Show only available products'
-          />
-        </AvailabilityFilter>
+        {status === 'open' && <Categories handleClick={handleSuggestionClick} />}
 
-        {status === 'open' && <Categories handleSuggestionClick={handleSuggestionClick} />}
-
-        {status === 'expanded' && <SearchSuggestions handleSuggestionClick={handleSuggestionClick} />}
+        {status === 'expanded' && <SearchSuggestions handleClick={handleSuggestionClick} />}
       </SearchWrapper>
 
       <Backdrop
         sx={{ color: '#fff', zIndex: 1 }}
         open={status !== 'closed'}
-        onClick={closeSearchBar}
+        onClick={() => dispatch(setSearchBarStatus('closed'))}
       />
     </SearchContainer>
   )
